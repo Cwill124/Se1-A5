4141e1aa62624aa5930573a035e9380d27aef42f
Corey Williams <cwill124@my.westga.edu>
2022-11-18 01:49:43 -0500

    should fail

--- code/HiLowGame/src/test/java/edu/westga/cs3211/hi_low_game/test/view/guess_report_textifier/TestTextifyGuessReport.java
+++ code/HiLowGame/src/test/java/edu/westga/cs3211/hi_low_game/test/view/guess_report_textifier/TestTextifyGuessReport.java
@@ -6,41 +6,41 @@ import org.junit.jupiter.api.Test;
 import edu.westga.cs3211.hi_low_game.view.GuessReportTextifier;
 import edu.westga.cs3211.hi_low_game.model.GuessReport;
 
-public class TestTextifyGuessReport  {
+public class TestTextifyGuessReport {
 
 	@Test
-	public void testTextifyTooHighMethod() {
+	public void TestTextifyTooHighMethod() {
 		GuessReportTextifier textifier = new GuessReportTextifier();
-		
+
 		String result = textifier.textifyGuessReport(GuessReport.TOO_HIGH);
-		
-		assertEquals(result, GuessReportTextifier.TOO_HIGH_TEXT, "checking result");
+
+		assertEquals(result,GuessReportTextifier.TOO_HIGH_TEXT,"checking result");
 	}
 
 	@Test
 	public void testTextifyTooLow() {
 		GuessReportTextifier textifier = new GuessReportTextifier();
-		
+
 		String result = textifier.textifyGuessReport(GuessReport.TOO_LOW);
-		
+
 		assertEquals(result, GuessReportTextifier.TOO_LOW_TEXT, "checking result");
 	}
 
 	@Test
 	public void testTextifyCorrect() {
 		GuessReportTextifier textifier = new GuessReportTextifier();
-		
+
 		String result = textifier.textifyGuessReport(GuessReport.CORRECT);
-		
+
 		assertEquals(result, GuessReportTextifier.CORRECT_TEXT, "checking result");
 	}
 
 	@Test
 	public void testTextifyGuessLimitReached() {
 		GuessReportTextifier textifier = new GuessReportTextifier();
-		
+
 		String result = textifier.textifyGuessReport(GuessReport.GUESS_LIMIT_REACHED);
-		
+
 		assertEquals(result, GuessReportTextifier.GUESS_LIMIT_REACHED_TEXT, "checking result");
 	}
 }
